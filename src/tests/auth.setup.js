6dbf1eb281c10fef3820f5a8b8f9b080137c2e3f
olena11o <alifanova.o@gmail.com>
2024-07-03 20:46:59 +0300

    auth-setup-problem

--- src/tests/auth.setup.js
+++ src/tests/auth.setup.js
@@ -1,22 +1,38 @@
 import { test as setup } from '@playwright/test';
+import { loginPage, inventoryPage} from '../fixture';
 
 const authFile = 'playwright/.auth/user.json';
 
-setup('authenticate', async ({ page }) => {
+setup('authenticate', async () => {
   // Perform authentication steps. Replace these actions with your own.
-  await page.goto('');
-  await page.locator('#user-name').fill('standard_user');
-  await page.locator('#password').fill('secret_sauce');
-  await page.locator('#login-button').click();
+  await loginPage.navigate();
+  await loginPage.performLogin('standard_user', 'secret_sauce');
 
   // Wait until the page receives the cookies.
   //
   // Sometimes login flow sets cookies in the process of several redirects.
   // Wait for the final URL to ensure that the cookies are actually set.
- await page.waitForURL('inventory.html');
+ await inventoryPage.waitForURL();
 
 
   // End of authentication steps.
 
-  await page.context().storageState({ path: authFile });
+  await inventoryPage.context().storageState({ path: authFile });
+
+//   // Perform authentication steps. Replace these actions with your own.
+//   await page.goto('');
+//   await page.locator('#user-name').fill('standard_user');
+//   await page.locator('#password').fill('secret_sauce');
+//   await page.locator('#login-button').click();
+
+//   // Wait until the page receives the cookies.
+//   //
+//   // Sometimes login flow sets cookies in the process of several redirects.
+//   // Wait for the final URL to ensure that the cookies are actually set.
+//  await page.waitForURL('inventory.html');
+
+
+//   // End of authentication steps.
+
+//   await page.context().storageState({ path: authFile });
 });
