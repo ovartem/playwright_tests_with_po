b9726702b94dbf9032950400e8dfb7831c90a584
Kateryna-art <bentleyss148@gmail.com>
2024-09-28 21:25:05 +0200

    Sorting test with Parametrize

---
+++ src/tests/sorting.spec.js
@@ -0,0 +1,28 @@
+import { expect } from '@playwright/test';
+import { test } from '../fixtures/base';
+import { users } from '../test-data/users';
+
+test.describe('Inventory Sorting Tests', () => {
+    test.beforeEach(async ({ app }) => {
+        const { username, password } = users.standardUser;
+        await app.login.navigate();
+        await app.login.performLogin(username, password);
+    });
+
+    const sortOptions = [
+        ['Name (A to Z)'],
+        ['Name (Z to A)'],
+        ['Price (low to high)'],
+        ['Price (high to low)'],
+    ];
+        sortOptions.forEach(([sortByLabel]) => {
+    test(`Sort items by ${sortByLabel}`, async ( /** @type {{ app: import('../pages/Application').Application }} */ { app }) => {
+        const originalNames = await app.inventory.getItemNames();
+        const originalPrices = await app.inventory.getItemPrices(); 
+
+        await app.inventory.selectSortOption(sortByLabel);
+        await app.inventory.verifySortedItemsByOption(sortByLabel);
+
+        });
+    });
+});
